27e13ed7816fab4a3ed90a2f2b84d9a6f3cb7df9
Ankitkumar-tech <95867896+Ankitkumar-tech@users.noreply.github.com>
2022-07-01 17:18:53 +0530

    covid-tracker

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './App.css';
+import { CovidTracker } from './covidtracker/CovidTracker';
+// import { Button } from './components/Button';
+
+function App() {
+  return (
+    <div className="App">
+    <CovidTracker/>
+
+    </div>
+  );
+}
+
+export default App;
